c2b83a352068c26e68aeb306b19871e2c955a81a
wycmeepo <1825038989@qq.com>
2021-01-01 16:39:27 +0800

    登录界面 注册界面 -更改样式

--- front-end/src/components/LoginPage.vue
+++ front-end/src/components/LoginPage.vue
@@ -1,31 +1,34 @@
 <template>
   <div id="login">
-    <el-header
-      ><el-page-header @back="goBack" content="登录页面"> </el-page-header
-    ></el-header>
     <el-row>
-      <el-col :span="12" :offset="6" 
-        ><el-main>
-          <el-form ref="loginForm" :model="loginForm" label-width="80px">
-            <el-form-item label="用户名" prop="username">
-              <el-input
-                v-model="loginForm.username"
-                placeholder="请输入用户名"
-              ></el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop="password">
-              <el-input
-                v-model="loginForm.password"
-                placeholder="请输入密码"
-                show-password
-              ></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="submit()">确认</el-button>
-              <el-button @click="clear('loginForm')">重置</el-button>
-            </el-form-item>
-          </el-form>
-        </el-main>
+      <el-page-header @back="goBack" content="登录页面"> </el-page-header>
+    </el-row>
+    <el-row>
+      <el-col :span="10" :offset="7">
+        <el-form
+          ref="loginForm"
+          :model="loginForm"
+          :rules="rules"
+          label-width="80px"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input
+              v-model="loginForm.username"
+              placeholder="请输入用户名"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input
+              v-model="loginForm.password"
+              placeholder="请输入密码"
+              show-password
+            ></el-input>
+          </el-form-item>
+          <div>
+            <el-button type="primary" @click="submit()">确认</el-button>
+            <el-button @click="clear('loginForm')">重置</el-button>
+          </div>
+        </el-form>
         <div class="grid-content bg-purple-dark"></div
       ></el-col>
     </el-row>
@@ -41,8 +44,28 @@ export default {
     return {
       loginForm: {
         username: '',
-        password: ''
-      }
+        password: '',
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          {
+            min: 6,
+            max: 20,
+            message: '长度在 4 到 20 位',
+            trigger: ['blur', 'change'],
+          },
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            min: 6,
+            max: 20,
+            message: '长度在 6 到 20 位',
+            trigger: ['blur', 'change'],
+          },
+        ],
+      },
     }
   },
   methods: {
@@ -51,123 +74,142 @@ export default {
       console.log('go back')
     },
     submit() {
-      if(this.checkUsername(this.loginForm.username) && this.checkPassword(this.loginForm.password)){
+      if (
+        this.checkUsername(this.loginForm.username) &&
+        this.checkPassword(this.loginForm.password)
+      ) {
         this.$axios
-        .post('http://localhost:8080/user/verify', {
-          username: this.loginForm.username,
-          password: this.loginForm.password
-        })
-        .then(res => {
-          let code = res.data['code']
-          let msg = res.data['msg']
-          let user = res.data['userLoginData']
-          if (code == 100) {
-            this.$message({
-              message: msg,
-              type: 'success'
-            })
-            this.$store.state.isLogin = true
-            this.$store.commit('setUser', user)
-            this.$router.replace('/mainpage')
-          } else if (code == 101) {
-            this.$message({
-              message: msg,
-              type: 'error'
-            })
-          } else if (code == 102) {
-            this.$message({
-              message: msg,
-              type: 'error'
-            })
-          } else {
-            this.$message({
-              message: 'please check your net connection',
-              type: 'error'
-            })
-          }
-        })
+          .post('http://localhost:8080/user/verify', {
+            username: this.loginForm.username,
+            password: this.loginForm.password,
+          })
+          .then((res) => {
+            let code = res.data['code']
+            let msg = res.data['msg']
+            let user = res.data['userLoginData']
+            if (code == 100) {
+              this.$message({
+                message: msg,
+                type: 'success',
+              })
+              this.$store.state.isLogin = true
+              this.$store.commit('setUser', user)
+              this.$router.replace('/mainpage')
+            } else if (code == 101) {
+              this.$message({
+                message: msg,
+                type: 'error',
+              })
+            } else if (code == 102) {
+              this.$message({
+                message: msg,
+                type: 'error',
+              })
+            } else {
+              this.$message({
+                message: 'please check your net connection',
+                type: 'error',
+              })
+            }
+          })
       }
     },
     clear(formName) {
       this.$refs[formName].resetFields()
     },
-    checkUsername(str){
-      if(str.length < 4 || str.length > 20){
-        this.$message ({
+    checkUsername(str) {
+      if (str.length < 4 || str.length > 20) {
+        this.$message({
           message: '用户名长度须在4-20位之间',
-          type: 'error'
+          type: 'error',
         })
-        return false;
+        return false
       }
-      var char = 0;
-      var num = 0;
-      for(var i=0; i<str.length; i++){
-        let c = str.charCodeAt(i);
-        if(c >= 97 && c <= 122 || c >= 65 && c <= 90){
-          char = 1;
+      var char = 0
+      var num = 0
+      for (var i = 0; i < str.length; i++) {
+        let c = str.charCodeAt(i)
+        if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90)) {
+          char = 1
         } else {
-          if(c >= 48 && c <= 58){
-            num = 1;
+          if (c >= 48 && c <= 58) {
+            num = 1
           } else {
-            this.$message ({
+            this.$message({
               message: '用户名存在非法字符',
-              type: 'error'
+              type: 'error',
             })
-            return false;
+            return false
           }
         }
       }
-      if(char==1 && num==1){
-        return true;
+      if (char == 1 && num == 1) {
+        return true
       } else {
-        this.$message ({
+        this.$message({
           message: '用户名必须由字母和数字组成',
-          type: 'error'
+          type: 'error',
         })
-        return false;
+        return false
       }
     },
-    checkPassword(str){
-       if(str.length < 6 || str.length > 20){
-        this.$message ({
+    checkPassword(str) {
+      if (str.length < 6 || str.length > 20) {
+        this.$message({
           message: '密码长度须在6-20位之间',
-          type: 'error'
+          type: 'error',
         })
-        return false;
+        return false
       }
-      var char1 = 0;
-      var char2 = 0;
-      var num = 0;
-      for(var i=0; i<str.length; i++){
-        let c = str.charCodeAt(i);
-        if(c >= 97 && c <= 122){
-          char1 = 1;
+      var char1 = 0
+      var char2 = 0
+      var num = 0
+      for (var i = 0; i < str.length; i++) {
+        let c = str.charCodeAt(i)
+        if (c >= 97 && c <= 122) {
+          char1 = 1
         } else {
-          if(c >= 65 && c <= 90){
-            char2 = 1;
+          if (c >= 65 && c <= 90) {
+            char2 = 1
           } else {
-            if(c >= 48 && c <= 58){
-              num = 1;
+            if (c >= 48 && c <= 58) {
+              num = 1
             } else {
-              this.$message ({
+              this.$message({
                 message: '密码存在非法字符',
-                type: 'error'
+                type: 'error',
               })
-              return false;
+              return false
             }
           }
         }
       }
-      if(char1==1 && char2==1 && num==1){
-        return true;
+      if (char1 == 1 && char2 == 1 && num == 1) {
+        return true
       } else {
-        this.$message ({
+        this.$message({
           message: '密码必须由大小字母和数字组成',
-          type: 'error'
+          type: 'error',
         })
-        return false;
+        return false
       }
     },
-  }
+  },
 }
 </script>
+
+<style>
+.el-page-header {
+  background-color: #b3c0d1;
+  height: 50px;
+  line-height: 50px;
+  margin-bottom: 30px;
+}
+
+.el-form {
+  background-color: #e9eef3;
+  line-height: 50px;
+  padding: 50px;
+  text-align: center;
+}
+</style>
--- front-end/src/components/SignUpPage.vue
+++ front-end/src/components/SignUpPage.vue
@@ -1,40 +1,41 @@
 <template>
   <div id="signup">
-    <el-header
-      ><el-page-header @back="goBack" content="注册页面"> </el-page-header
-    ></el-header>
     <el-row>
-      <el-col :span="12" :offset="6"
-        ><el-main>
-          <el-form ref="signUpForm" :model="signUpForm" :rules="rules" label-width="80px">
-            <el-form-item label="用户名" prop="username">
-              <el-input
-                v-model="signUpForm.username"
-                placeholder="请输入用户名(4-20位,由数字和字母组成)"
-              ></el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop="password">
-              <el-input
-                v-model="signUpForm.password"
-                placeholder="请输入密码(6-20位,由数字和大小字母组成)"
-                show-password
-              ></el-input>
-            </el-form-item>
-            <el-form-item label="确认密码" prop="rePassword">
-              <el-input
-                v-model="signUpForm.rePassword"
-                placeholder="请再次输入密码"
-                show-password
-              ></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="submit()"
-                >确认</el-button
-              >
-              <el-button @click="clear('signUpForm')">重置</el-button>
-            </el-form-item>
-          </el-form>
-        </el-main>
+      <el-page-header @back="goBack" content="注册页面"> </el-page-header>
+    </el-row>
+    <el-row>
+      <el-col :span="10" :offset="7">
+        <el-form
+          ref="signUpForm"
+          :model="signUpForm"
+          :rules="rules"
+          label-width="80px"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input
+              v-model="signUpForm.username"
+              placeholder="请输入用户名(4-20位,由数字和字母组成)"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input
+              v-model="signUpForm.password"
+              placeholder="请输入密码(6-20位,由数字和大小字母组成)"
+              show-password
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="rePassword">
+            <el-input
+              v-model="signUpForm.rePassword"
+              placeholder="请再次输入密码"
+              show-password
+            ></el-input>
+          </el-form-item>
+          <div>
+            <el-button type="primary" @click="submit()">确认</el-button>
+            <el-button @click="clear('signUpForm')">重置</el-button>
+          </div>
+        </el-form>
         <div class="grid-content bg-purple-dark"></div
       ></el-col>
     </el-row>
@@ -49,107 +50,123 @@ export default {
       signUpForm: {
         username: '',
         password: '',
-        rePassword: ''
+        rePassword: '',
       },
       rules: {
         username: [
-          { required: true, message: '请输入用户名', trigger: 'blur'},
-          { min: 6, max: 20, message: '长度在 4 到 20 位', trigger: ['blur', 'change'] }
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          {
+            min: 6,
+            max: 20,
+            message: '长度在 4 到 20 位',
+            trigger: ['blur', 'change'],
+          },
         ],
         password: [
           { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 6, max: 20, message: '长度在 6 到 20 位', trigger: ['blur', 'change'] }
+          {
+            min: 6,
+            max: 20,
+            message: '长度在 6 到 20 位',
+            trigger: ['blur', 'change'],
+          },
         ],
         rePassword: [
-          { required: true, message: '请再次输入密码', trigger: 'blur' }
-        ]
-      }
+          { required: true, message: '请再次输入密码', trigger: 'blur' },
+        ],
+      },
     }
   },
   methods: {
-    goBack () {
+    goBack() {
       this.$router.go(-1)
       console.log('go back')
     },
-    submit () {
-      if (this.checkUsername(this.signUpForm.username) && this.checkPassword(this.signUpForm.password)) {
+    submit() {
+      if (
+        this.checkUsername(this.signUpForm.username) &&
+        this.checkPassword(this.signUpForm.password)
+      ) {
         if (this.signUpForm.password == this.signUpForm.rePassword) {
           const form = new FormData()
           form.append('username', this.signUpForm.username)
           form.append('password', this.signUpForm.password)
           this.$axios
-          .post("http://localhost:8080/user/signUp", new URLSearchParams(form))
-          .then(res => {
-              let code = res.data['code'];
-              let msg = res.data['msg'];
-              if(code == 200) {
-                  this.$message({
-                    message: msg,
-                    type: 'success'
-                  });
-                  this.$router.replace("/mainpage");
+            .post(
+              'http://localhost:8080/user/signUp',
+              new URLSearchParams(form)
+            )
+            .then((res) => {
+              let code = res.data['code']
+              let msg = res.data['msg']
+              if (code == 200) {
+                this.$message({
+                  message: msg,
+                  type: 'success',
+                })
+                this.$router.replace('/mainpage')
               }
-              if(code == 201) {
-                  this.$message({
-                    message: msg,
-                    type: 'error'
-                  });
+              if (code == 201) {
+                this.$message({
+                  message: msg,
+                  type: 'error',
+                })
               }
             })
         } else {
-          this.$message ({
+          this.$message({
             message: '两次输入密码不一致',
-            type: 'error'
+            type: 'error',
           })
         }
       }
     },
-    clear (formName) {
+    clear(formName) {
       this.$refs[formName].resetFields()
     },
-    checkUsername(str){
-      if(str.length < 4 || str.length > 20){
-        this.$message ({
+    checkUsername(str) {
+      if (str.length < 4 || str.length > 20) {
+        this.$message({
           message: '用户名长度须在4-20位之间',
-          type: 'error'
+          type: 'error',
         })
-        return false;
+        return false
       }
       var char = 0
       var num = 0
       for (var i = 0; i < str.length; i++) {
         let c = str.charCodeAt(i)
-        if (c >= 97 && c <= 122 || c >= 65 && c <= 90) {
+        if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90)) {
           char = 1
         } else {
           if (c >= 48 && c <= 58) {
             num = 1
           } else {
-            this.$message ({
+            this.$message({
               message: '用户名存在非法字符',
-              type: 'error'
+              type: 'error',
             })
-            return false;
+            return false
           }
         }
       }
       if (char == 1 && num == 1) {
         return true
       } else {
-        this.$message ({
+        this.$message({
           message: '用户名必须由字母和数字组成',
-          type: 'error'
+          type: 'error',
         })
-        return false;
+        return false
       }
     },
-    checkPassword(str){
-       if(str.length < 6 || str.length > 20){
-        this.$message ({
+    checkPassword(str) {
+      if (str.length < 6 || str.length > 20) {
+        this.$message({
           message: '密码长度须在6-20位之间',
-          type: 'error'
+          type: 'error',
         })
-        return false;
+        return false
       }
       var char1 = 0
       var char2 = 0
@@ -165,11 +182,11 @@ export default {
             if (c >= 48 && c <= 58) {
               num = 1
             } else {
-              this.$message ({
+              this.$message({
                 message: '密码存在非法字符',
-                type: 'error'
+                type: 'error',
               })
-              return false;
+              return false
             }
           }
         }
@@ -177,13 +194,29 @@ export default {
       if (char1 == 1 && char2 == 1 && num == 1) {
         return true
       } else {
-        this.$message ({
+        this.$message({
           message: '密码必须由大小字母和数字组成',
-          type: 'error'
+          type: 'error',
         })
-        return false;
+        return false
       }
-    }
-  }
+    },
+  },
 }
 </script>
+
+<style>
+.el-page-header {
+  background-color: #b3c0d1;
+  height: 50px;
+  line-height: 50px;
+  margin-bottom: 30px;
+}
+
+.el-form {
+  background-color: #e9eef3;
+  line-height: 50px;
+  padding: 50px;
+  text-align: center;
+}
+</style>

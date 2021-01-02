fb55f8ccc66cd9538a60ff162d9f3a19f78de2b9
wycmeepo <1825038989@qq.com>
2021-01-02 23:06:30 +0800

    解决冲突

--- front-end/src/components/TextPage.vue
+++ front-end/src/components/TextPage.vue
@@ -21,6 +21,23 @@
         </div>
       </el-col>
     </el-row>
+
+    <el-row>
+      <el-col :span="14" :offset="5" id="textpage-main">
+        <el-col :span="21"
+          ><el-input v-model="input" placeholder="发表你的评论"></el-input
+        ></el-col>
+        <!-- TODO 记录登录的人发表的评论 -->
+        <el-col :span="2" :offset="1"
+          ><el-button type="primary" round>发表</el-button></el-col
+        >
+        <div id="textpage-comment" v-for="item in commentList" :key="item.id">
+          <p>{{ item.text }}</p>
+          <p>{{ item.time }}</p>
+          <p>{{ item.writer }}</p>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -32,7 +49,14 @@ export default {
   name: 'Text',
   data: () => {
     return {
-      article: store.state.readingNow
+      article: store.state.readingNow,
+      input: '',
+      commentList: [
+        { id: 0, text: '123', writer: 'aa', time: '1.1' },
+        { id: 1, text: '234', writer: 'bb', time: '2.2' },
+        { id: 2, text: '345', writer: 'cc', time: '3.3' },
+        { id: 3, text: '456', writer: 'dd', time: '4.4' },
+      ],
     }
   },
   methods: {
@@ -51,7 +75,7 @@ export default {
 }
 </script>
 
-<style >
+<style>
 .el-page-header {
   background-color: #b3c0d1;
   height: 50px;
@@ -59,19 +83,20 @@ export default {
   margin-bottom: 30px;
 }
 
-#main {
+#textpage-main {
   background-color: #ffffff;
   padding: 20px;
   box-shadow: 10px 10px 30px #777777;
+  margin-bottom: 10px;
 }
 
-#title {
+#textpage-title {
   text-align: center;
   font-weight: bold;
   font-size: 30px;
 }
 
-#autor {
+#textpage-autor {
   background-color: #e9eef3;
   text-align: end;
   font-size: 18px;
@@ -80,11 +105,18 @@ export default {
   margin: 10px;
 }
 
-#content {
+#textpage-content {
   margin: 10px;
+}
+
+#textpage-comment {
+  margin-top: 50px;
 }
 
 p {
   display: inline;
+}
+
+#textpage-comment p {
 }
 </style>

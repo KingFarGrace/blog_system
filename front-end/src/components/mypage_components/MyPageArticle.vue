47e0d9f40318d6b99d4a2de61fd3ebdee3fbd252
zhongshiwei456 <18894616424@163.com>
2021-01-04 23:02:44 +0800

    个人信息布局框架初版

--- front-end/src/components/mypage_components/MyPageArticle.vue
+++ front-end/src/components/mypage_components/MyPageArticle.vue
@@ -1,51 +1,53 @@
 <template>
   <div>
-  <el-table
-    :data="tableData.filter(data => !search || data.title.toLowerCase().includes(search.toLowerCase()))"
-    style="width: 100%" >
-    <el-table-column
-      label="Date"
-      prop="date">
-    </el-table-column>
-    <el-table-column
-      label="Title"
-      prop="title">
-    </el-table-column>
+    <el-table
+      :data="tableData.filter(data => !search || data.title.toLowerCase().includes(search.toLowerCase()))"
+      style="width: 100%">
+      <el-table-column
+        label="Date"
+        prop="date">
+      </el-table-column>
+      <el-table-column
+        label="Title"
+        prop="title">
+      </el-table-column>
 
-    <el-table-column
-      align="right">
-      <template slot="header">
-        <el-input
-          v-model="search"
-          size="mini"
-          placeholder="输入关键字搜索"/>
-      </template>
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+      <el-table-column
+        align="right">
+        <template slot="header" slot-scope="scope">
+          <el-input
+            v-model="search"
+            size="mini"
+            placeholder="输入关键字搜索"/>
+        </template>
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">Edit
+          </el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">Delete
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 
-  <el-pagination
-    background
-    layout="prev, pager, next"
-    page-size="6"
-    :total="totalpage"
-    @current-change="pagechange">     //response the paging click
-  </el-pagination>
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      page-size="6"
+      :total="totalpage"
+      @current-change="pagechange"> //response the paging click
+    </el-pagination>
   </div>
 </template>
 
 <script>
   export default {
     created() {     //database request
-      const _this = this
+      const _this = this;
       axios.post('        ').then(function (resp) {  //TODO 填写存储发表文章的数据库url，获取文章的title、body与总页数
         //console.log(resp)
         //console.log(resp.data)
@@ -55,22 +57,22 @@
     },
     data() {
       return {
-        totalpage:null,
+        totalpage: null,
         tableData: [
-        //   {
-        //   date: '',
-        //   title: ''
-        // },
+          //   {
+          //   date: '',
+          //   title: ''
+          // },
           {
-          date: '2016-05-04',
-          title: '论大学生宿舍日常'
-        }, {
-          date: '2016-05-01',
-          title: '论母猪的产后护理'
-        }, {
-          date: '2016-05-03',
-          title: '雨季不再来'
-        }
+            date: '2016-05-04',
+            title: '论大学生宿舍日常'
+          }, {
+            date: '2016-05-01',
+            title: '论母猪的产后护理'
+          }, {
+            date: '2016-05-03',
+            title: '雨季不再来'
+          }
         ],
         search: ''
       }
@@ -78,9 +80,9 @@
     methods: {
       handleEdit(index, row) {
         this.$router.push({
-          path:'/mypage/edit',
-          query:{
-            id : row.id
+          path: '/mypage/edit',
+          query: {
+            id: row.id
           }
         })
       },
@@ -90,14 +92,13 @@
         // })
         this.tableData.splice(index, 1);
       },
-      pagechange(currentPage){  //Get information on each page
+      pagechange(currentPage) {  //Get information on each page
         alert(currentPage)
         // const _this = this
         // axios.post('        '+currentPage+' ' ).then(function (resp) {     //TODO 得到‘currentPage’页的信息,语句不知道是否正确
         //   _this.tableData = resp.data.content
         // })
       }
-
 
 
     }
--- front-end/src/components/mypage_components/MyPageBase.vue
+++ front-end/src/components/mypage_components/MyPageBase.vue
@@ -1,29 +1,69 @@
 <template>
+  <el-card class="box-card">
+    <el-form ref="form" :model="form" label-width="100px">
+      <el-row>
+        <el-col :span="8">
+          <el-form-item label="昵称">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col span="8">
+          <el-form-item label="ID">
+            <el-input v-model="form.id" disabled></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
 
-  <el-form ref="form" :model="form" label-width="100px">
-    <el-col :span="12">
-    <el-form-item label="昵称">
-      <el-input v-model="form.name" disabled></el-input>
-    </el-form-item>
-    <el-form-item label="账号">
-      <el-input v-model="form.id" disabled></el-input>
-    </el-form-item>
-    <el-form-item label="创建时间">
-      <el-input v-model="form.Date" disabled></el-input>
-    </el-form-item>
-    <el-form-item label="发表文章数">
-      <el-input v-model="form.number" disabled></el-input>
-    </el-form-item>
-<!--    <el-form-item label="个性签名">-->
-<!--      <el-input v-model="form.introduction" autosize></el-input>-->
-<!--    </el-form-item>-->
+      <el-row>
+        <el-col :span="8">
+          <el-form-item label="发表文章数">
+            <el-input v-model="form.number"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col span="4">
+          <el-form-item label="性别">
+            <el-radio v-model="form.gender" label="1">男生</el-radio>
+            <el-radio v-model="form.gender" label="2">女生</el-radio>
+          </el-form-item>
+        </el-col>
+        <el-col span="4">
+          <el-form-item label="年龄">
+            <el-input v-model="form.age"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
 
-      <el-form-item>
-      <el-button type="primary" @click="onPassword">修改密码</el-button>
-    </el-form-item>
-    </el-col>
+      <el-row>
+        <el-col :span="8">
+          <el-form-item label="邮箱">
+            <el-input v-model="form.mailAddress"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col span="8">
+          <el-form-item label="注册时间">
+            <el-input v-model="form.Date" disabled></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="15">
+          <el-form-item label="个性签名">
+            <el-input v-model="form.signature"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
 
-  </el-form>
+      <el-row>
+        <el-col :offset="8">
+          <el-button class="button1" plain @click="submit()">保存信息</el-button>
+          <el-button class="button2" plain @click="onPassword">修改密码</el-button>
+        </el-col>
+      </el-row>
+
+    </el-form>
+
+  </el-card>
+
 
 </template>
 
@@ -31,24 +71,49 @@
   export default {
     data() {
       return {
+        // radio: '1',
         form: {
           name: '',
-          id:'',
-          Date:'',
-          number:'',
-          introduction:'',
+          id: '',
+          age: '',
+          number: '',
+          Date: '',
+          gender: '',
+          mailAddress: '',
+          signature: '',
         }
       }
     },
     methods: {
       onPassword() {
         this.$router.push({
-          path:'/mypage/password',
+          path: '/mypage/password',
         })
       },
 
       //TODO 缺少从后端获取个人基本信息的函数
 
+      submit(){  //TODO 将form中的信息传入数据库，（性别gender 传字符，‘1’男‘2’女）
+
+      },
+
+      checkEmail() {
+        var regEmail = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+        if (this.form.mailAddress != '' && !regEmail.test(this.form.mailAddress)) {
+          this.$message({
+            message: '邮箱格式不正确',
+            type: 'error'
+          });
+          this.form.mailAddress = ''
+        }
+      }
+
     }
   }
 </script>
+
+
+<style>
+
+
+</style>
